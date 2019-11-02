98c87a6b0b323d39bc32b33af0ad3194b3f20d00
jonathasacruz <jonathas.acruz@gmail.com>
2019-11-02 19:48:14 -0300

    Códigos de Leandro movidos para a pasta src

--- src/agenda/InterfaceUsuario.java
+++ src/agenda/InterfaceUsuario.java
@@ -1,5 +1,15 @@
-package agenda;
+
 
 public interface InterfaceUsuario {
+	
+	public void ConstruirMenu (String opcoesMenu[]);
+	
+	// NÃO SEI SE PRECISA DE UM MÉTODO PARA CADA MENU
+	
+	/*public void ConstruirMenuAgendamento (String opcoesMenuAgendamento[]);
+	public void ConstruirMenuConsulta (String opcoesMenuConsulta[]);
+	public void ConstruirMenuListar (String opcoesMenuListar[]);
+	public void ConstruirMenuCancelar (String opcoesMenuCancelar[]);
+	public void exibirMensagem(String msg, Mensagem mensagem);*/
 
 }
